2b217af878274c5d5c92bde6db84efc0ad121471
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-18 17:50:18 +0300

    test

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,16 +6,6 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-
-        int x1 = 0;
-        int x2 = 1;
-        int[] result = new int[n];
-        for (int i = 0; i < n; ++i) {
-            result[i] = x1;
-            int sum = x1 + x2;
-            x1 = x2;
-            x2 = sum;
-        }
-        return result;
+        return null;
     }
 }

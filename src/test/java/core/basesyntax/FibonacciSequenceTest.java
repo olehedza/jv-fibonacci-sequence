6beeb0409abdf0f2bc5705f95f4e27e3ffeb0118
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-20 11:36:57 +0300

    made fibonacciSequence as static final field

--- src/test/java/core/basesyntax/FibonacciSequenceTest.java
+++ src/test/java/core/basesyntax/FibonacciSequenceTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class FibonacciSequenceTest {
-    private int[] fibonacciSequence = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+    private static final int[] fibonacciSequence = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
             610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368};
 
     @Test

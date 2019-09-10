9c083894a67538ef5e2c608e9faa156ff4088d28
Sofasmile <sofiya.dyakonova@ukr.net>
2019-09-10 10:50:42 +0300

    added recursion

--- src/test/java/core/basesyntax/FibonacciSequenceTest.java
+++ src/test/java/core/basesyntax/FibonacciSequenceTest.java
@@ -3,21 +3,19 @@ package core.basesyntax;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class FibonacciSequenceTest {
-    private static final int[] fibonacciSequence = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
-            233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368};
+    private static final int[] FIBONACCI_SEQUENCE = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+            233, 377, 610, 987, 1597, 2584, 4181};
 
     @Test
     public void getFibonacciSequence() {
         FibonacciSequence sequence = new FibonacciSequence();
         for (int i = 0; i < 20; i++) {
-            int[] temp = fibonacciSequence;
-            int[] actualResult = sequence.getFibonacciSequence(i);
-            int[] expectedResult = Arrays.copyOf(temp, i);
-            Assert.assertArrayEquals(
-                    "Test failed with parameter: " + i, expectedResult, actualResult);
+            int actualResult = sequence.getFibonacciNumber(i);
+            int expectedResult = FIBONACCI_SEQUENCE[i];
+            Assert.assertEquals(
+                    "Test failed with number in the position: " + i,
+                    expectedResult, actualResult);
         }
     }
 }

3c5bab12f004fe9f17c64a67d36549ba1e4aa53b
Sofasmile <sofiya.dyakonova@ukr.net>
2019-09-11 17:44:19 +0300

    removed static

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -9,7 +9,7 @@ public class FibonacciSequence {
      * <p>Пример: n = 8
      * Результат: 21</p>
      */
-    public static int getFibonacciNumber(int index) {
+    public int getFibonacciNumber(int index) {
         return 0;
     }
 }

82d0c872095357cd948f6e85b7cb35a882b88072
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-09-11 17:44:52 +0300

    added recursion (#36)
    * added recursion
    
    * removed static

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -2,10 +2,14 @@ package core.basesyntax;
 
 public class FibonacciSequence {
     /**
-     * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
-     * из `n`чисел  последовательности Фибоначчи.
+     * <p>Реализуйте метод `getFibonacciNumber(int n)` который возвращает число Фибоначи которое
+     * стоит на `n` позиции в последовательности Фибоначчи (начало нумерации с нуля).
+     * Используйте рекурсию.</p>
+     *
+     * <p>Пример: n = 8
+     * Результат: 21</p>
      */
-    public int[] getFibonacciSequence(int n) {
-        return null;
+    public int getFibonacciNumber(int index) {
+        return 0;
     }
 }
